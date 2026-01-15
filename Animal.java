0ff0695e922b57859b2b44cfd7a04d60b07c9359
Haiiden <hlowhlowh@gmail.com>
2026-01-15 13:16:08 -0500

    super + chicken constructor

--- Animal.java
+++ Animal.java
@@ -26,9 +26,30 @@ public abstract class Animal{
     this.hp = newHP;
   }
 
+  public int getSpeed(){
+    return speed;
+  }
+  public void setSpeed(int newSpeed){
+    this.speed = newSpeed;
+  }
+
+  public int getAttack(){
+    return attack;
+  }
+  public void setAttack(int newAttack){
+    this.attack = newAttack;
+  }
+
+  public int gethp(){
+    return hp;
+  }
+  public void sethp(int newHP){
+    this.hp = newHP;
+  }
+
+
+
   public abstract String moveOne(Animal opp);
   public abstract String moveTwo(Animal opp);
   public abstract String moveThree(Animal opp);
-
-
 }
--- Chicken.java
+++ Chicken.java
@@ -1,1 +1,8 @@
-public cl
+public class Chicken extends Animal{
+  public Chicken (name){
+    super(name);
+    sethp(5);
+    setSpeed(20);
+    setAttack(10);
+  }
+}
