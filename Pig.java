88657ca47609324a5772a6df64e7c28b29f972c3
Stanley-0210 <stanleyh35@nycstudents.net>
2026-01-16 18:06:55 +0000

    Update Pig.java

--- Pig.java
+++ Pig.java
@@ -25,7 +25,7 @@ public class Pig extends Animal{
     mudLevel = n;
   }
 
-  /*Deal 2-7 damage to opponent, increases mud level*/
+  /*roll*/
   public String moveOne(Animal other){
     int damage = (int)(Math.random()*6)+2;
     other.applyDamage((damage + (damage * mudLevel * 0.2)) * this.attack);
@@ -34,7 +34,7 @@ public class Pig extends Animal{
     return this + " the Pig rolled on "+ other + " and dealt "+ damage +
     " points of damage.";
   }
-  /*Deal 3-12 damage to opponent
+  /*bite
   */
   public String moveTwo(Animal other){
       setSpecial(getSpecial()-8);
@@ -45,10 +45,10 @@ public class Pig extends Animal{
       return this + " bit "+other+" dealing "+ damage +" points of damage.";
   }
   
-  /*Restores 6 special and 1 hp to self.*/
+  /*eat*/
   public String moveThree(){
     int hp = 5;
-    setHP(getHP()+hp);
+    sethp(gethp()+hp);
     return this+" eats to restore "+ hp +" but mud level reset";
   }
 }
