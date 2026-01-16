2bf36d0c5efe6dd3e3a6cf871199e6aa7a93a6e5
Stanley-0210 <stanleyh35@nycstudents.net>
2026-01-16 17:57:43 +0000

    change names

--- Pig.java
+++ Pig.java
@@ -29,17 +29,17 @@ public class Pig extends Animal{
   }
 
   /*Deal 2-7 damage to opponent, increases mud level*/
-  public String roll(Animal other){
+  public String moveOne(Animal other){
     int damage = (int)(Math.random()*6)+2;
     other.applyDamage(damage + (damage * mudLevel * 0.2));
     int prevMud = this.mudLevel;
     this.setSpecial(prevMud + 1);
-    return this + " the Pig attacked "+ other + " and dealt "+ damage +
+    return this + " the Pig rolled on "+ other + " and dealt "+ damage +
     " points of damage.";
   }
   /*Deal 3-12 damage to opponent
   */
-  public String bite(Animal other){
+  public String moveTwo(Animal other){
       setSpecial(getSpecial()-8);
       int damage = (int)(Math.random()*5+Math.random()*5)+3;
       other.applyDamage(damage + (damage * mudLevel * 0.2);
@@ -49,7 +49,7 @@ public class Pig extends Animal{
   }
   
   /*Restores 6 special and 1 hp to self.*/
-  public String eat(){
+  public String moveThree(){
     int hp = 5;
     setHP(getHP()+hp);
     return this+" eats to restore "+ hp +" but mud level reset";
