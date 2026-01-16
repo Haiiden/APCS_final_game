1d4454e1f845a08826b067768c2adab851ec95f1
Stanley-0210 <stanleyh35@nycstudents.net>
2026-01-16 18:01:45 +0000

    Change constructors
    Refactor Pig constructor and update damage calculation in move methods.

--- Pig.java
+++ Pig.java
@@ -1,15 +1,12 @@
 public class Pig extends Animal{
   int mudLevel;
 
-  /*the other constructors ultimately call the constructor
-    with all parameters.*/
-  public Pig(String name, int hp){
-    super(name,hp);
-    mudLevel = 1;
-  }
-
   public Pig(String name){
-    this(name,15);
+    super(name);
+    this.hp = 15;
+    this.speed = 5;
+    this.attack = 1.2;
+    this.mudLevel = 1;
   }
 
   public Pig(){
@@ -31,7 +28,7 @@ public class Pig extends Animal{
   /*Deal 2-7 damage to opponent, increases mud level*/
   public String moveOne(Animal other){
     int damage = (int)(Math.random()*6)+2;
-    other.applyDamage(damage + (damage * mudLevel * 0.2));
+    other.applyDamage((damage + (damage * mudLevel * 0.2)) * this.attack);
     int prevMud = this.mudLevel;
     this.setSpecial(prevMud + 1);
     return this + " the Pig rolled on "+ other + " and dealt "+ damage +
@@ -42,7 +39,7 @@ public class Pig extends Animal{
   public String moveTwo(Animal other){
       setSpecial(getSpecial()-8);
       int damage = (int)(Math.random()*5+Math.random()*5)+3;
-      other.applyDamage(damage + (damage * mudLevel * 0.2);
+      other.applyDamage((damage + (damage * mudLevel * 0.2)) * this.attack);
       int prevMud = this.mudLevel;
       this.setSpecial(prevMud + 1);
       return this + " bit "+other+" dealing "+ damage +" points of damage.";
