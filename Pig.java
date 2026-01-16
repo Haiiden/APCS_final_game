1d47aa5867d589f5bd24d35f9379704aa71fe50f
Stanley-0210 <stanleyh35@nycstudents.net>
2026-01-16 17:50:23 +0000

    added more moves

--- Pig.java
+++ Pig.java
@@ -31,38 +31,27 @@ public class Pig extends Animal{
   /*Deal 2-7 damage to opponent, increases mud level*/
   public String roll(Animal other){
     int damage = (int)(Math.random()*6)+2;
-    other.applyDamage(damage * this.mudLevel);
+    other.applyDamage(damage + (damage * mudLevel * 0.2));
     int prevMud = this.mudLevel;
     this.setSpecial(prevMud + 1);
     return this + " the Pig attacked "+ other + " and dealt "+ damage +
-    " points of damage. Their mud level increased.";
+    " points of damage.";
   }
-  /*Deal 3-12 damage to opponent, only if caffeine is high enough.
-    Reduces caffeine by 8.
+  /*Deal 3-12 damage to opponent
   */
-  public String specialAttack(Adventurer other){
-    if(getSpecial() >= 8){
+  public String bite(Animal other){
       setSpecial(getSpecial()-8);
       int damage = (int)(Math.random()*5+Math.random()*5)+3;
-      other.applyDamage(damage);
-      return this + " used their "+preferredLanguage+
-      " skills to hack the matrix. "+
-      " This glitched out "+other+" dealing "+ damage +" points of damage.";
-    }else{
-      return "Not enough caffeine to use the ultimate code. Instead "+attack(other);
-    }
+      other.applyDamage(damage + (damage * mudLevel * 0.2);
+      int prevMud = this.mudLevel;
+      this.setSpecial(prevMud + 1);
+      return this + " bit "+other+" dealing "+ damage +" points of damage.";
   }
-
-  /*Restores 5 special to other*/
-  public String support(Adventurer other){
-    return "Gives a coffee to "+other+" and restores " 
-    +other.restoreSpecial(5)+" "+other.getSpecialName();
-  }
+  
   /*Restores 6 special and 1 hp to self.*/
-  public String support(){
-    int hp = 1;
+  public String eat(){
+    int hp = 5;
     setHP(getHP()+hp);
-    return this+" drinks a coffee to restores "+restoreSpecial(6)+" "
-    + getSpecialName()+ " and "+hp+" HP";
+    return this+" eats to restore "+ hp +" but mud level reset";
   }
 }
